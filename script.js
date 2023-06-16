d422942573be23ffbb460453a1de8ca53c6ea1e0
Prosper Banda <prosperbanda@meta.com>
2023-06-16 15:49:21 -0700

    Added functionality to the buttons, successfully fetched the required endpoint data

--- script.js
+++ script.js
@@ -1,21 +1,97 @@
 console.log("Hello!")
 
-var submitBtn = document.getElementById("submitBtn");
-var searchMovie = document.getElementById("searchMovie");
-var moviesDiv = document.getElementById("moviesDiv");
-var loadMoreBtn = document.getElementById("loadMoreBtn");
+// Declare a variable to store the api key
+const apiKey = '9a346f17bae1f4a51b997cf9ddff71f1';
+const baseUrl = 'https://api.themoviedb.org/3';
 
-// Add event listeners to the buttons
-submitBtn.addEventListener("click", handleSubmit);
-loadMoreBtn.addEventListener('click', handleLoadMore);
+var clearBtn = document.getElementById("clear-button");
 
-// Define the functions to be executed when the buttons are clicked
-function handleSubmit(event) {
-    event.preventDefault();
 
-    console.log('Submit button clicked!');
+let page = 1;
+let currentMovies = [];
+
+//Fetch the list of current movies from TMDB API
+async function fetchCurrentMovies(){
+    try{
+        const response = await fetch(`${baseUrl}/movie/now_playing?api_key=${apiKey}&page=${page}`);
+        const data = await response.json();
+        currentMovies = currentMovies.concat(data.results);
+        displayMovies(data.results);
+        page++;
+    } catch(error){
+        console.log(error);
+    }
 }
 
-function handleLoadMore() {
-  console.log('Load more button clicked!');
+//Display movies on the webpage
+function displayMovies(movies){
+    const moviesList = document.getElementById('movies-list');
+    
+    movies.forEach(movie => {
+        const movieItem = document.createElement('li');
+        movieItem.className = 'movie-item';
+
+        const moviePoster = document.createElement('img');
+
+
+
+        moviePoster.src = `https://image.tmdb.org/t/p/w200/${movie.poster_path}`;
+        moviePoster.alt = movie.title;
+        moviePoster.className = 'movie-poster';
+        movieItem.appendChild(moviePoster);
+       
+        const movieDetails = document.createElement('div');
+        movieDetails.innerHTML = `
+        <h2 class="movie-title">${movie.title}</h2>
+        <p class="movie-votes">Votes: ${movie.vote_count}</p>
+      `;
+
+        movieItem.appendChild(moviePoster);
+        movieItem.appendChild(movieDetails);
+        moviesList.appendChild(movieItem);
+
+    });
+
+    //Load more movies when the Load More Button is clicked
+    document.getElementById('load-more-button').addEventListener('click', fetchCurrentMovies);
+    console.log("Button clicked!");
+
+    //Search for movies
+    document.getElementById('search-form').addEventListener('submit', function(event){
+        event.preventDefault();
+        const searchInput = document.getElementById('search-input');
+        const searchTerm = searchInput.value.trim();
+        if (searchTerm !== ''){
+            searchMovies(searchTerm);
+            searchInput.value = '';
+        }
+    });
+
+    //Clear search results and display previous current movies
+    document.getElementById('clear-button').addEventListener('click', function(){
+        displayMovies(currentMovies);
+        console.log("Button clicked!");
+    });
+
+    //Search movies using the API
+    async function searchMovies(query){
+        try{
+            const response = await fetch(`${baseUrl}/search/movie?api_key=${apiKey}&query=${query}`);
+            const data = await response.json();
+            const searchResults = data.results;
+
+            const divElement = document.getElementById('movies-list');
+            divElement.innerHTML = ''; 
+            displayMovies(searchResults);
+
+               
+        } catch(error){
+            console.log(error);
+        }
+
+    }
 }
+
+//Fetch initial movies
+fetchCurrentMovies();
+
